c4a99b9699790f4b623f473736b9b62ce33cd1cf
Joshua <jssmiller94@gmail.com>
2021-04-01 12:36:47 +0100

    on a roll

--- functions_lab_2/start_code/task_list.py
+++ functions_lab_2/start_code/task_list.py
@@ -1,3 +1,5 @@
+import pdb
+
 tasks = [
     { "description": "Wash Dishes", "completed": False, "time_taken": 10 },
     { "description": "Clean Windows", "completed": False, "time_taken": 15 },
@@ -7,10 +9,36 @@ tasks = [
 ]
 # 1  Print a list of uncompleted tasks
 
-def uncompleted_tasks(list, completed):
+def uncompleted_tasks(list):
+    
+    jobs = []
     for task in tasks:
         if task['completed'] == False:
-            return task
+            jobs.append(task['description'])
+    return jobs
 
-print(uncompleted_tasks(tasks, completed))
+print(uncompleted_tasks(tasks))
 
+# 2. Print a list of completed tasks
+
+def completed_tasks(list):
+    
+    jobs = []
+    for task in tasks:
+        if task['completed'] == True:
+            jobs.append(task['description'])
+    return jobs
+
+print(completed_tasks(tasks))
+
+
+# 3. Print a list of all task descriptions
+
+def all_tasks(list):
+    
+    jobs = []
+    for task in tasks:
+        jobs.append(task['description'])
+    return jobs
+
+print(all_tasks(tasks))
