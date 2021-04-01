99d375346675a36f2d78fcf3f975190c397dcdd6
Joshua <jssmiller94@gmail.com>
2021-04-01 12:02:19 +0100

    task 1, first attempt

---
+++ functions_lab_2/start_code/task_list.py
@@ -0,0 +1,15 @@
+tasks = [
+    { "description": "Wash Dishes", "completed": False, "time_taken": 10 },
+    { "description": "Clean Windows", "completed": False, "time_taken": 15 },
+    { "description": "Make Dinner", "completed": True, "time_taken": 30 },
+    { "description": "Feed Cat", "completed": False, "time_taken": 5 },
+    { "description": "Walk Dog", "completed": True, "time_taken": 60 },
+]
+# 1  Print a list of uncompleted tasks
+
+def uncompleted_tasks(list, completed):
+    jobs = []
+    for task in tasks:
+        if 'completed' == False:
+            return task
+
